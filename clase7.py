630e8fd4f3ea00d7115c60e425cebbe8f37138b4
mariakinstler <mariakinstler@iresm.edu.ar>
2026-04-22 13:03:30 -0300

    Update clase7.py

--- clase7.py
+++ clase7.py
@@ -1,1 +1,14 @@
+# Crear la clase de personaje
+class Personaje:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
 
+
+# Crear el objeto
+harry_potter = Personaje("Harry Potter", 19)
+
+# Mostrar datos
+print("Nombre:", harry_potter.nombre)
+print("Edad:", harry_potter.edad)
+print ("Hola soy Harry Potter bienvenido a mi programa ")
