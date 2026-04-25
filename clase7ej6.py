0f0b45d5483ab8d94f9066f9cc60bc67d93f5857
mariakinstler <mariakinstler@iresm.edu.ar>
2026-04-25 11:46:11 -0300

    clase7ej6.py

---
+++ clase7ej6.py
@@ -0,0 +1,11 @@
+class Cubo:
+    # Atributo de clase
+    caras = 6
+
+    def __init__(self, color):
+        # Atributo de instancia
+        self.color = color
+
+
+cubo_rojo = Cubo("rojo")
+print("Mi cubo es de color rojo ")
