17352e09930ad138c034b473100f38c0b3ea625f
mariakinstler <mariakinstler@iresm.edu.ar>
2026-04-25 11:59:04 -0300

    clase7ejer2metodo.py

---
+++ clase7ejer2metodo.py
@@ -0,0 +1,10 @@
+class Mago:
+    def lanzar_hechizo(self):
+        print("¡Abracadabra!")
+
+# Crear la instancia de Mago
+merlin = Mago()
+
+# Hacer que lance un hechizo
+merlin.lanzar_hechizo()
+print("Mi mago hizo abracadabra y desaparecio")
