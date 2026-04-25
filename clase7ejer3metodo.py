12702d3801bf9d4b793cc57da3069227f9bcfe0d
mariakinstler <mariakinstler@iresm.edu.ar>
2026-04-25 12:00:43 -0300

    clase7ejer3metodo.py

---
+++ clase7ejer3metodo.py
@@ -0,0 +1,9 @@
+class Alarma:
+    def postergar(self, cantidad_minutos):
+        print(f"La alarma ha sido pospuesta {cantidad_minutos} minutos")
+
+# Crear una instancia de Alarma
+mi_alarma = Alarma()
+
+
+mi_alarma.postergar(10)
